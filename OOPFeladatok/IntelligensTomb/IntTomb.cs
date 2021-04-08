d4a3b383c910a8b7283dc5fed2209c11d8df8b3c
pepe1973 <bodrogipr@gmail.com>
2021-04-08 13:20:17 +0200

    Első kommitálás

---
+++ OOPFeladatok/IntelligensTomb/IntTomb.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPFeladatok.IntelligensTomb
+{
+    class IntTomb
+    {
+        public int[] IntegerTomb { get; set; }
+        public int Elemszam { get; set; }
+        public int IntervallumKezdo { get; set; }
+        public int IntervallumVeg { get; set; }
+
+        public IntTomb(int elemszam, int intervallumKezdo, int intervallumVeg)
+        {
+            this.Elemszam = elemszam;
+            this.IntervallumKezdo = intervallumKezdo;
+            this.IntervallumVeg = intervallumVeg;
+            this.IntegerTomb = new int[this.Elemszam];
+            SajatFeltoltes();
+        }
+
+        public void SajatFeltoltes()
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < this.Elemszam; i++)
+            {
+                this.IntegerTomb[i] = rnd.Next(this.IntervallumKezdo, this.IntervallumVeg + 1);
+            }
+        }
+
+        public void SajatAdatokKiirasa()
+        {
+            foreach (int elem in this.IntegerTomb)
+            {
+                Console.WriteLine(elem);
+            }
+        }
+
+        public int Osszegszamol()
+        {
+            int osszeg = 0;
+
+            foreach (int elem in this.IntegerTomb)
+            {
+                osszeg += elem;
+            }
+
+            return osszeg;
+        }
+
+        public double Atlagszamol()
+        {
+            double atlag = (double)Osszegszamol() / this.Elemszam;
+
+            return atlag;
+        }
+    }
+}
---
+++ OOPFeladatok/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OOPFeladatok.IntelligensTomb;
+
+namespace OOPFeladatok
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IntTomb it = new IntTomb(5, 10, 20);
+            it.SajatAdatokKiirasa();
+
+            Console.ReadKey(true);
+        }
+    }
+}
